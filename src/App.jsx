eb1e725ecd2cbcce05347730e5e942176d81e6c3
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-09-21 07:19:57 -0700

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+// App.jsx
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LustreToaster } from './components/lustreToaster'
+
 import HomePage from './pages/homePage'
 import About from './pages/aboutPage'
 import Contact from './pages/contact'
@@ -14,39 +16,35 @@ import NotFoundPage from './pages/notFoundPage'
 import AdminPage from './pages/adminPage'
 import User from './pages/user'
 
-
-
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="w-full h-[100vh]">
+        <div className="w-full min-h-screen">
           <Routes>
-            <Route path="/*" element={<HomePage />} />
-            <Route path="/home" element={<HomePage />} />
+            {/* Public Routes */}
+            <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgotPassword" element={<ForgotPassword />} />
             <Route path="/resetPassword" element={<ResetPassword />} />
-            <Route path="/admin/*" element={<AdminPage />} />
-            <Route path="/user" element={<User />} />
-            <Route path="*" element={<NotFoundPage />} />
             <Route path="/dateAndTimeSelect" element={<DateAndTimeSelect />} />
             <Route path="/appointment" element={<Appointment />} />
+            <Route path="/user" element={<User />} />
+
+
+            <Route path="/admin/*" element={<AdminPage />} />
+
+            {/* Catch-all 404 */}
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
       </BrowserRouter>
 
-
-
-
-
       {/* Custom Toaster */}
       <LustreToaster />
-
-
     </>
   )
 }
